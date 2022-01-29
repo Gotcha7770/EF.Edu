cf81d8107835c17aaede07bdd2699fa72a2da3ed
bakanov-g <bakanov-g@speechpro.com>
2022-01-29 13:21:56 +0300

    in memory db tests example

--- EF.Tests/Model/Item.cs
+++ EF.Tests/Model/Item.cs
@@ -1,4 +1,18 @@
 namespace EF.Tests.Model
 {
-    public record Item(int Id, string Name);
+    public class Item
+    {
+        public Item(int id, string name, int amount)
+        {
+            Id = id;
+            Name = name;
+            Amount = amount;
+        }
+
+        public int Id { get; }
+
+        public string Name { get; set; }
+
+        public int Amount { get; set; }
+    }
 }
