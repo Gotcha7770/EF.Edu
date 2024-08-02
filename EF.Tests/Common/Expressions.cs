fec1ec56a3077105220a060131f469e364ad34a4
Gotcha7770 <gbakanov.box@gmail.com>
2024-08-02 12:19:06 +0300

    expressions composition example

--- EF.Tests/Common/Expressions.cs
+++ EF.Tests/Common/Expressions.cs
@@ -9,7 +9,8 @@ namespace EF.Tests.Common;
 
 public static class Expressions
 {
-    public static Expression<Func<T, TConverted>> Convert<T, TResult, TConverted>(this Expression<Func<T, TResult>> expr)
+    public static Expression<Func<T, TConverted>> Convert<T, TResult, TConverted>(
+        this Expression<Func<T, TResult>> expr)
     {
         return Expression.Lambda<Func<T, TConverted>>(
             Expression.Convert(expr, typeof(TConverted)), expr.Parameters);
@@ -30,24 +31,11 @@ public static class Expressions
         return left.Compose(right, Expression.OrElse);
     }
 
-    // public static Expression<Func<TIn, TOut>> Compose<TIn, TInOut, TOut>(
-    //     this Expression<Func<TIn, TInOut>> input,
-    //     Expression<Func<TInOut, TOut>> inOutOut)
-    // {
-    //     var parameter = Expression.Parameter(typeof(TIn));
-    //
-    //     return Expression.Lambda<Func<TIn, TOut>>(
-    //         Expression.Invoke(
-    //             inOutOut,
-    //             Expression.Invoke(input, parameter)), 
-    //         parameter);
-    // }
-    
     /// <summary>
     /// Combines the first expression with the second using the specified merge function.
     /// </summary>
     public static Expression<T> Compose<T>(
-        this Expression<T> first, 
+        this Expression<T> first,
         Expression<T> second,
         Func<Expression, Expression, Expression> merge)
     {
@@ -55,10 +43,10 @@ public static class Expressions
         var map = first.Parameters
             .Select((x, i) => new { First = x, Second = second.Parameters[i] })
             .ToDictionary(x => x.Second, p => p.First);
- 
+
         // create a merged lambda expression with parameters from the first expression
         return Expression.Lambda<T>(
-            merge(first.Body, ParameterRebinder.ReplaceParameters(map, second.Body)), 
+            merge(first.Body, ParameterRebinder.ReplaceParameters(map, second.Body)),
             first.Parameters);
     }
 
@@ -66,29 +54,14 @@ public static class Expressions
         this Expression<Func<TIn, TIntermediate>> propertyAccessor,
         Expression<Func<TIntermediate, TOut>> predicate)
     {
-        // Expression expression = predicate.Body switch
-        // {
-        //     BinaryExpression binaryExpression => binaryExpression.Update(
-        //         (binaryExpression.Left as MemberExpression).Wrap(propertyAccessor.Body),
-        //         binaryExpression.Conversion,
-        //         binaryExpression.Right),
-        //     MethodCallExpression methodCallExpression => methodCallExpression.Update(
-        //         methodCallExpression.Object,
-        //         [
-        //             methodCallExpression.Arguments[0],
-        //             (methodCallExpression.Arguments[1] as MemberExpression).Wrap(propertyAccessor.Body)
-        //         ])
-        // };
+        var parameter = Expression.Parameter(typeof(TIn));
 
-        var expression = new PropertyEditor(typeof(TIntermediate), x => x.Wrap(propertyAccessor.Body)).Visit(predicate);
+        // возвращаем лямбду нужного типа
         return Expression.Lambda<Func<TIn, TOut>>(
-            (expression as LambdaExpression).Body,
-            propertyAccessor.Parameters[0]);
-    }
-
-    public static MemberExpression Wrap(this MemberExpression first, Expression second)
-    {
-        return Expression.Property(second, first.Member.Name);
+            Expression.Invoke(
+                predicate,
+                Expression.Invoke(propertyAccessor, parameter)),
+            parameter);
     }
 
     public class ParameterRebinder : ExpressionVisitor
@@ -110,41 +83,20 @@ public static class Expressions
                 exp);
         }
 
-        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+        public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map,
+            Expression exp)
         {
             return new ParameterRebinder(map).Visit(exp);
         }
- 
+
         protected override Expression VisitParameter(ParameterExpression parameter)
         {
             if (_map.TryGetValue(parameter, out var replacement))
             {
                 parameter = replacement;
             }
- 
+
             return base.VisitParameter(parameter);
-        }
-    }
-
-    public class PropertyEditor : ExpressionVisitor
-    {
-        private readonly Type _entityType;
-        private readonly Func<MemberExpression, MemberExpression> _selector;
-
-        public PropertyEditor(Type entityType, Func<MemberExpression, MemberExpression> selector)
-        {
-            _entityType = entityType;
-            _selector = selector;
-        }
-
-        protected override Expression VisitMember(MemberExpression node)
-        {
-            if (node.Expression is ParameterExpression parameter && parameter.Type == _entityType)
-            {
-                return _selector(node);
-            }
-
-            return base.VisitMember(node);
         }
     }
 }
--- EF.Tests/ExpressionsComposeTest.cs
+++ EF.Tests/ExpressionsComposeTest.cs
@@ -11,8 +11,6 @@ namespace EF.Tests;
 
 public class ExpressionsComposeTest
 {
-    private static readonly string[] Companies = ["WB", ""];
-
     [Fact]
     public async Task Compose()
     {
@@ -24,15 +22,12 @@ public class ExpressionsComposeTest
                 .Generate());
 
         //Expression<Func<Company, bool>> companySpec = x => x.Name == "WB";
-        Expression<Func<Company, bool>> companySpec = x => !Companies.Contains(x.Name);
-        Expression<Func<Person, bool>> personSpec = x => x.Company.Name == company.Name;
+        //Expression<Func<Company, bool>> companySpec = x => !Companies.Contains(x.Name);
+        Expression<Func<Company, bool>> companySpec = x => x.Address.City != "Moscow";
+        // Expression<Func<Person, bool>> personSpec = x => x.Company.Name == company.Name;  <- duplicate!
 
         var companyQuery = dbContext.Companies.Where(companySpec);
-        //var personQuery = dbContext.Persons.Where(x => x.Company.Name != "ADT" && x.Company.Name != "MMM");
-        // var personQuery = dbContext.Persons
-        //     .Where(x => x.Company.Address.City != "Moscow")
-        //     .Where(x => x.Company.Address.City != "Tokyo");
-        //var personQuery = dbContext.Persons.Where(x => companySpec(x.Company)); can't do this
+        // var personQuery = dbContext.Persons.Where(x => companySpec(x.Company)); <- can't do this
         var personQuery = dbContext.Persons.Where(x => x.Company, companySpec);
 
         var sql = personQuery.ToQueryString();
