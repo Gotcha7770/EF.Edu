d96d22320843c1d02ea3f63df2845f7529fb7f45
Gotcha7770 <gbakanov.box@gmail.com>
2024-09-19 16:48:58 +0300

    добавил бенчмарк для AsNoTracking

---
+++ EF.Benchmarks/AsNoTrackingBench.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+using EF.Benchmarks.Entities;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF.Benchmarks;
+
+[MemoryDiagnoser]
+public class AsNoTrackingBench
+{
+    private SqliteConnection _connection;
+
+    [Params(1, 100, 1000)]
+    public int ItemsCount { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        // База данных SQLite в памяти (Filename=:memory:) живет только в рамках одного соединения.
+        _connection = new SqliteConnection(BenchmarkDbContext.InMemoryConnectionString);
+        using var context = BenchmarkDbContextDesignFactory.CreateDbContext(_connection);
+        _connection.Open();
+        
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        var items = Fakes.SaleFaker.GenerateLazy(ItemsCount);
+        context.Sales.AddRange(items);
+        context.SaveChanges();
+    }
+
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+        _connection.Close();
+    }
+
+    [Benchmark]
+    public Task<Sale[]> With_Tracking()
+    {
+        using var context = BenchmarkDbContextDesignFactory.CreateDbContext(_connection);
+        return context.Sales
+            .ToArrayAsync();
+    }
+
+    [Benchmark]
+    public Task<Sale[]> Without_Tracking()
+    {
+        using var context = BenchmarkDbContextDesignFactory.CreateDbContext(_connection);
+        return context.Sales
+            .AsNoTracking()
+            .ToArrayAsync();
+    }
+}
