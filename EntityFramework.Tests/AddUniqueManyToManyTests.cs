413f6535a55f55e90c1d677bea0198525c7ef483
Gotcha7770 <gbakanov.box@gmail.com>
2025-09-27 23:36:52 +0300

    Upsert

--- EntityFramework.Tests/AddUniqueManyToManyTests.cs
+++ EntityFramework.Tests/AddUniqueManyToManyTests.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AwesomeAssertions;
-using EFCore.BulkExtensions;
 using EntityFramework.Common.Model;
 using EntityFramework.Tests.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Xunit;
 using static AwesomeAssertions.FluentActions;
 
@@ -90,22 +91,52 @@ public class AddUniqueManyToManyTests
     }
 
     [Fact]
-    public async Task AddDuplicate_UsingFind()
+    public async Task AddDuplicate_UsingChangeTracker()
     {
         await using var dbContext = TestDbContextFactory.Create(TestDbContextFactory.LocalPostgresDbOptions);
 
         var result = await dbContext.AddAsync(_route1);
         await dbContext.SaveChangesAsync();
 
-        dbContext.AddOrUpdateRange(
-            _route2.Segments,
-            x => [x.Carrier, x.FlightNumber, x.DepartureDate]);
-        result = await dbContext.AddAsync(_route2);
-        //result = await dbContext.AddOrUpdateAsync(_route2.Segments[0]);
-        await dbContext.SaveChangesAsync();
+        await using var transaction = await dbContext.Database.BeginTransactionAsync();
+        try
+        {
+            foreach (var segment in _route2.Segments)
+            {
+                await dbContext.Database.ExecuteSqlAsync(
+                    $"""
+                         INSERT INTO "Segments" 
+                         ("StartCode", "DepartureDate", "EndCode", "ArrivalDate", "Carrier", "FlightNumber")
+                         VALUES ({segment.StartCode}, {segment.DepartureDate}, {segment.EndCode}, {segment.ArrivalDate}, {segment.Carrier}, {segment.FlightNumber})
+                         ON CONFLICT ("Carrier", "FlightNumber", "DepartureDate") DO NOTHING;
+                     """);
+
+                var tracked = dbContext.ChangeTracker.Entries<Segment>()
+                    .FirstOrDefault(e =>
+                        e.Entity.Carrier == segment.Carrier &&
+                        e.Entity.FlightNumber == segment.FlightNumber &&
+                        e.Entity.DepartureDate == segment.DepartureDate);
+                
+                if (tracked != null)
+                {
+                    tracked.State = EntityState.Detached;
+                }
+
+                //dbContext.Detach(segment);
+                dbContext.Entry(segment).State = EntityState.Unchanged;
+            }
+
+            result = dbContext.Routes.Add(_route2);
+            await dbContext.SaveChangesAsync();
+            await transaction.CommitAsync();
+        }
+        catch (Exception ex)
+        {
+            await transaction.RollbackAsync();
+        }
 
         result.Entity.Should().BeEquivalentTo(_route2);
-        
+
         dbContext.Routes
             .Should()
             .BeEquivalentTo([_route1, _route2], opt => opt.Excluding(x => x.Segments));
@@ -136,95 +167,66 @@ public class AddUniqueManyToManyTests
             .BeEquivalentTo(new HashSet<Segment>([.._route1.Segments, .._route2.Segments]));
     }
 
-    [Fact]
-    public async Task AddDuplicate_UsingBulkInsert()
-    {
-        await using var dbContext = TestDbContextFactory.Create(TestDbContextFactory.LocalPostgresDbOptions);
-
-        var result = await dbContext.AddAsync(_route1);
-        await dbContext.SaveChangesAsync();
-
-        // https://entityframework-extensions.net/bulk-insert#common-options-in-entity-framework-extensions
-        // https://github.com/videokojot/EFCore.BulkExtensions.MIT
-
-        await dbContext.BulkInsertOrUpdateAsync(_route2.Segments, options =>
-        {
-            // options.OnConflictUpdateWhereColumns = new[] { "Carrier", "FlightNumber", "DepartureDate" };
-            // options.IgnoreOnConflict = true; // чтобы не вставлять дубликаты
-        });
-        
-        dbContext.Detach(_route2.Segments);
-        result = await dbContext.AddAsync(_route2);
-        foreach (var segment in _route2.Segments)
-        {
-            dbContext.Entry(segment).State = EntityState.Unchanged;
-        }
-        
-        await dbContext.SaveChangesAsync();
-        
-        result.Entity.Should().BeEquivalentTo(_route2);
-
-        dbContext.Routes
-            .Should()
-            .BeEquivalentTo([_route1, _route2], opt => opt.Excluding(x => x.Segments));
-
-        dbContext.Segments
-            .Should()
-            .BeEquivalentTo(new HashSet<Segment>([.._route1.Segments, .._route2.Segments]));
-    }
-
     private async Task<EntityEntry<Route>> AddUsingRawSql(TestDbContext dbContext, Route route)
     {
         await using var transaction = await dbContext.Database.BeginTransactionAsync();
 
-        const string insertRouteSql =
-            """
-                INSERT INTO "Routes" ("Id", "StartCode", "DepartureDate", "EndCode")
-                VALUES ({0}, {1}, {2}, {3})
-            """;
-
-        await dbContext.Database.ExecuteSqlRawAsync(
-            insertRouteSql,
-            route.Id,
-            route.StartCode,
-            route.DepartureDate,
-            route.EndCode);
-
-        foreach (var segment in route.Segments)
+        try
         {
-            const string insertSegmentSql =
+            const string insertRouteSql =
                 """
-                    INSERT INTO "Segments" ("Carrier", "FlightNumber", "DepartureDate", "StartCode", "ArrivalDate", "EndCode")
-                    VALUES ({0}, {1}, {2}, {3}, {4}, {5})
-                    ON CONFLICT ("Carrier", "FlightNumber", "DepartureDate") DO NOTHING;
-                """;
-
-            await dbContext.Database.ExecuteSqlRawAsync(
-                insertSegmentSql,
-                segment.Carrier,
-                segment.FlightNumber,
-                segment.DepartureDate,
-                segment.StartCode,
-                segment.ArrivalDate,
-                segment.EndCode);
-
-            const string insertJoinSql =
-                """
-                    INSERT INTO "RouteSegment" ("RouteId", "SegmentsCarrier", "SegmentsFlightNumber", "SegmentsDepartureDate")
+                    INSERT INTO "Routes" ("Id", "StartCode", "DepartureDate", "EndCode")
                     VALUES ({0}, {1}, {2}, {3})
                 """;
 
             await dbContext.Database.ExecuteSqlRawAsync(
-                insertJoinSql,
+                insertRouteSql,
                 route.Id,
-                segment.Carrier,
-                segment.FlightNumber,
-                segment.DepartureDate);
+                route.StartCode,
+                route.DepartureDate,
+                route.EndCode);
+
+            foreach (var segment in route.Segments)
+            {
+                const string insertSegmentSql =
+                    """
+                        INSERT INTO "Segments" ("Carrier", "FlightNumber", "DepartureDate", "StartCode", "ArrivalDate", "EndCode")
+                        VALUES ({0}, {1}, {2}, {3}, {4}, {5})
+                        ON CONFLICT ("Carrier", "FlightNumber", "DepartureDate") DO NOTHING;
+                    """;
+
+                await dbContext.Database.ExecuteSqlRawAsync(
+                    insertSegmentSql,
+                    segment.Carrier,
+                    segment.FlightNumber,
+                    segment.DepartureDate,
+                    segment.StartCode,
+                    segment.ArrivalDate,
+                    segment.EndCode);
+
+                const string insertRouteToSegmentSql =
+                    """
+                        INSERT INTO "RouteSegment" ("RouteId", "SegmentsCarrier", "SegmentsFlightNumber", "SegmentsDepartureDate")
+                        VALUES ({0}, {1}, {2}, {3})
+                    """;
+
+                await dbContext.Database.ExecuteSqlRawAsync(
+                    insertRouteToSegmentSql,
+                    route.Id,
+                    segment.Carrier,
+                    segment.FlightNumber,
+                    segment.DepartureDate);
+            }
+
+            await transaction.CommitAsync();
+            
+            return dbContext.Entry(route);
         }
-
-        await transaction.CommitAsync();
-
-        return dbContext.Entry(route);
+        catch (Exception)
+        {
+            await transaction.RollbackAsync();
+            throw;
+        }
     }
 }
 
@@ -233,25 +235,49 @@ public static partial class AdHocExtensions
     /// <summary>
     /// Открепляет сущность от ChangeTracker.
     /// </summary>
-    public static void Detach<TEntity>(this DbContext dbContext, IEnumerable<TEntity> entities)
+    public static void Detach<TEntity>(this DbContext dbContext, params TEntity[] entities)
         where TEntity : class
     {
         foreach (var entity in entities)
         {
+            // var tmp = dbContext.Find<TEntity>("1");
+            // var manager = dbContext.GetService<IStateManager>();
             foreach (var tracked in dbContext.ChangeTracker.Entries<TEntity>())
             {
                 if (tracked.Entity.Equals(entity))
                 {
                     tracked.State = EntityState.Detached;
+                    break;
                 }
             }
         }
-        // var tracked = dbContext.ChangeTracker.Entries<TEntity>().ToArray();
-        // var entry = dbContext.Entry(entities);
-        // if (entry.State != EntityState.Detached)
-        // {
-        //     entry.State = EntityState.Detached;
-        // }
+    }
+    
+    /// <summary>
+    /// Наверно, эффективный, но не безопасный способ получить затреканный элемент из DbContext
+    /// </summary>
+    /// <param name="db"></param>
+    /// <param name="keyValues"></param>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <returns></returns>
+    public static EntityEntry<TEntity> TryGetTracked<TEntity>(this DbContext db, params object[] keyValues)
+        where TEntity : class
+    {
+        var entry = TryGetTracked(db, typeof(TEntity), keyValues);
+        return entry is null
+            ? null
+            : new EntityEntry<TEntity>(entry);
+    }
+
+    public static InternalEntityEntry TryGetTracked(this DbContext db, Type type, params object[] keyValues)
+    {
+        var stateManager = db.GetService<IStateManager>();
+        var entityType = db.Model.FindEntityType(type);
+        if (entityType is null)
+            throw new InvalidOperationException($"Entity type {type.Name} not found in the model.");
+        var key = entityType.FindPrimaryKey();
+        var entry = stateManager.TryGetEntry(key, keyValues);
+        return entry;
     }
 
     public static EntityEntry<TEntity> AddOrUpdateAsync<TEntity>(
@@ -262,11 +288,6 @@ public static partial class AdHocExtensions
         where TEntity : class
     {
         var keyValues = keyValuesSelector(entity);
-        // var attached = await dbContext.FindAsync<TEntity>(keyValues, cancellationToken);
-        // if (attached is null)
-        // {
-        //     return await dbContext.AddAsync(entity, cancellationToken);
-        // }
 
         // Проверяем, не трекается ли уже объект
         var tracked = dbContext.ChangeTracker
